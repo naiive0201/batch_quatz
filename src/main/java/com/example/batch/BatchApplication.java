61cc7a1374a53b1c804667402185763d79711d80
hyunsooju <hyunsooting@gmail.com>
2019-05-12 13:15:24 +0900

    #1 Replaced by Web Scrapping

--- src/main/java/com/example/batch/BatchApplication.java
+++ src/main/java/com/example/batch/BatchApplication.java
@@ -1,5 +1,6 @@
 package com.example.batch;
 
+import com.example.batch.job.WebScrappingJob;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
@@ -9,18 +10,21 @@ public class BatchApplication {
 
         Scheduler scheduler = null;
         JobDataMap jobDataMap = new JobDataMap();
+
         jobDataMap.put("WTF", "WTF, Guys Shut the front door");
-        JobDetail jobDetail = JobBuilder.newJob(TestJob.class)
-                .withIdentity(TestJob.class.getName(), "groupA")
+
+        JobDetail jobDetail = JobBuilder.newJob(WebScrappingJob.class)
+                .withIdentity(WebScrappingJob.class.getName(), "groupA")
                 .withDescription("WTF is the people")
                 .setJobData(jobDataMap)
                 .usingJobData("Key", 1.4443f)
                 .build();
 
         Trigger trigger = TriggerBuilder.newTrigger()
-                .withIdentity(TestJob.class.getName(), "groupA")
+                .withIdentity(WebScrappingJob.class.getName(), "groupA")
                 .withSchedule(SimpleScheduleBuilder.repeatMinutelyForTotalCount(30))
                 .build();
+
         try {
             scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.scheduleJob(jobDetail, trigger);
@@ -28,11 +32,6 @@ public class BatchApplication {
         } catch (SchedulerException se) {
             se.printStackTrace();
         }
-
-
-
-
-
     }
 
 }
